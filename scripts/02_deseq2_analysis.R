c0daa94c6cae89ab1ee081720ca383fae1babea4
Anastasiia Shcherbakova <anastasiia.shcherbakova@yahoo.com>
2025-04-14 17:48:22 +0200

    update of 02_deseq2: Move deletion of non-replicated samples from 02_deseq2 to 01_preprocessing

--- scripts/02_deseq2_analysis.R
+++ scripts/02_deseq2_analysis.R
@@ -1,56 +1,39 @@
-
 library(DESeq2)
 library(dplyr)
 
 counts <- read.csv("D:/Research/GEO/GSE146853/filtered_counts.csv", check.names = FALSE, row.names = 1)
-metadata <- read.csv("D:/Research/GEO/GSE146853/filtered_metadata.csv", check.names = FALSE)
-colnames(metadata) <- make.names(colnames(metadata), unique = TRUE)
-metadata <- metadata[, c("Disease", "Sample_ID")]
-metadata$Patient_ID <- sub("\\.\\d+$", "", metadata$Sample_ID)
+metadata <- read.csv("D:/Research/GEO/GSE146853/filtered_metadata.csv", check.names = FALSE, row.names = 1)
+head(counts)
+head(metadata)
 
-# Identify patients with exactly 2 replicates
-replicated <- metadata %>%
-  group_by(Patient_ID) %>%
-  tally() %>%
-  filter(n == 2) %>%
-  pull(Patient_ID)
-
-# Filter metadata
-metadata_filtered <- metadata %>%
-  filter(Patient_ID %in% replicated | Disease == "Healthy")
 
 # Output folder
 outdir <- "D:/Research/GEO/GSE146853/Results/DESeq2"
 dir.create(outdir, showWarnings = FALSE)
 
 # Loop through each patient (with replicates)
-patients_to_test <- metadata_filtered %>%
+patients_to_test <- metadata %>%
   filter(Disease != "Healthy") %>%
   pull(Patient_ID) %>%
   unique()
+patients_to_test
 
 for (patient_id in patients_to_test) {
-  disease <- metadata_filtered %>%
+  disease <- metadata %>%
     filter(Patient_ID == patient_id) %>%
     pull(Disease) %>%
     unique()
   
-  # Select samples: 2 for patient + all healthy
-  selected_samples <- metadata_filtered %>%
+  selected_samples <- metadata %>%
     filter(Disease == "Healthy" | Patient_ID == patient_id)
   
   sample_ids <- selected_samples$Sample_ID
   cond <- ifelse(selected_samples$Disease == "Healthy", "Healthy", disease)
   condition <- factor(cond, levels = c("Healthy", disease))
   
-  # Build colData
-  coldata <- data.frame(row.names = sample_ids,
-                        Condition = condition)
-  
-  # Subset count matrix
+  coldata <- data.frame(row.names = sample_ids, Condition = condition)
   counts_sub <- counts[, sample_ids]
   
-  # Run DESeq2
   dds <- DESeqDataSetFromMatrix(countData = round(counts_sub),
                                 colData = coldata,
                                 design = ~ Condition)
